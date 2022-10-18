3d78490189a6fec66b7edc3a9d2821d3f8a08ef4
StefanieLoeser <stefanie.loeser@gmx.net>
2022-10-18 13:44:31 +0200

    removed unnecessary questionmark

--- pages/recordbag.js
+++ pages/recordbag.js
@@ -44,7 +44,7 @@ export default function RecordBag({ onToggleBookmark, results, onSetResults }) {
                     }
                   />
                 ))
-            : collectionState?
+            : collectionState
                 .filter((record) => record.isChecked)
                 .map((file) => (
                   <RecordFile
