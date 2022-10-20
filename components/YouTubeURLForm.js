a3608c278bd25b6ccea908b145bf879415c3061c
StefanieLoeser <stefanie.loeser@gmx.net>
2022-10-20 13:35:05 +0200

    add clear input functionality

--- components/YouTubeURLForm.js
+++ components/YouTubeURLForm.js
@@ -10,6 +10,7 @@ export default function YouTubeURLForm({
 }) {
   const {
     register,
+    resetField,
     handleSubmit,
     formState: { errors },
   } = useForm({
@@ -32,6 +33,7 @@ export default function YouTubeURLForm({
         return record;
       });
       onSetCollection(collectionWithVideos);
+      resetField('youtubeVideoUrl');
     }
   };
 
